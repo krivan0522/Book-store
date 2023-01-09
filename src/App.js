f2c7b5f6c2c3849e2b9116d4474fdd46ea2d2583
Krish Agarwal <agarwalkrish29@gmail.com>
2023-01-09 21:10:53 +0530

    .

--- src/App.js
+++ src/App.js
@@ -11,11 +11,30 @@ import { UserContext } from './UserContext';
 import { useState } from 'react';
 import Cart from './Cart';
 import Home from './Home';
+import { QrReader } from "react-qr-reader";
 
 function App() {
   // const [value, setValue] = useState({"noqty":0, "books":[], "price":0})
   const [value, setValue] = useState({"noqty":0, "fp":0, "booktem":[]}) ;
+  const [selected, setSelected] = useState("environment");
+  const [startScan, setStartScan] = useState(false);
+  const [loadingScan, setLoadingScan] = useState(false);
+  const [data, setData] = useState("");
 
+  const handleScan = async (scanData) => {
+    setLoadingScan(true);
+    console.log(`loaded data data`, scanData);
+    if (scanData && scanData !== "") {
+      console.log(`loaded >>>`, scanData);
+      setData(scanData);
+      setStartScan(false);
+      setLoadingScan(false);
+      // setPrecScan(scanData);
+    }
+  };
+  const handleError = (err) => {
+    console.error(err);
+  };
 
   return (
     <Router>
@@ -25,6 +44,27 @@ function App() {
           <div className="navBar">
             <Navbar />
           </div>
+          <button className="button-landingpage" onClick={() => { setStartScan(!startScan); }}>
+          {startScan ? "Stop Scan" : "Start Scan"}
+        </button>
+        {startScan && (
+          <>
+            <select onChange={(e) => setSelected(e.target.value)}>
+              <option value={"environment"}>Back Camera</option>
+              <option value={"user"}>Front Camera</option>
+            </select>
+            <QrReader
+              facingMode={selected}
+              delay={1000}
+              onError={handleError}
+              onScan={handleScan}
+              // chooseDeviceId={()=>selected}
+              style={{ width: "300px" }}
+            />
+          </>
+        )}
+        {loadingScan && <p>Loading</p>}
+        {data !== "" && <p>{data}</p>}
         </div>
           <Routes>
             <Route path='/' element={<Home />}/>
